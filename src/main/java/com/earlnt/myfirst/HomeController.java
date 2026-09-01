0f7bb4276bbf88e2ac693a6b6861c7eb9f4f5ecc
Nolan Earl <nearl2828@gmail.com>
2026-09-01 15:52:46 -0400

    Update

--- src/main/java/com/earlnt/myfirst/HomeController.java
+++ src/main/java/com/earlnt/myfirst/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/contact")
+    @RequestMapping("/")
     public String home() {
         return "index.html";
     }
