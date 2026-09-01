1ded93204dab201fa2d8caa79a6122121b958a4b
Nolan Earl <nearl2828@gmail.com>
2026-09-01 16:30:48 -0400

    Added about page

--- src/main/java/com/earlnt/myfirst/HomeController.java
+++ src/main/java/com/earlnt/myfirst/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String home() {
         return "index.html";
     }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about.html";
+    }
 }
